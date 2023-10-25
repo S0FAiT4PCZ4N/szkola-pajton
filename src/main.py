1f99374560ad15c2f040f9eabb0d10c1aad7aeab
Seweryn Kozik <sewkozik@gmail.com>
2023-10-25 14:31:33 +0200

    Organise structure

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import matematyka
+
+a = 10
+b = 5
+
+print(f"{a} + {b} = {matematyka.dodaj(a, b)}")
+print(f"{a} - {b} = {matematyka.odejmij(a, b)}")
+print(f"{a} * {b} = {matematyka.iloczyn(a, b)}")
+print(f"{a} / {b} = {matematyka.iloraz(a, b)}")
